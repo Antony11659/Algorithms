4b9b188376c6571a1cbaa60d39e27ffc02e0a715
Anton Shurupov <ant_shur@LAPTOP-HJQGFM4B.localdomain>
2024-02-13 18:50:15 +0300

    my solution findMin and the best solution maxProdSubarr

--- array/maxProductSubarray.js
+++ array/maxProductSubarray.js
@@ -3,17 +3,42 @@
 
 // The test cases are generated so that the answer will fit in a 32-bit integer.
 
+// my solution O(n)
+// var maxProduct = function (nums) {
+//   let negProduct = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     const product = nums[i] * nums[i - 1];
+//     const minProduct = Math.min(nums[i], product);
+//     negProduct = nums[i] * negProduct;
+//     nums[i] = Math.max(Math.max(product, nums[i]), negProduct);
+//     negProduct = Math.min(minProduct, negProduct);
+//   }
+//   return Math.max(...nums);
+// };
+
+// better solution O(n) only more elegant and less memory requires
 var maxProduct = function (nums) {
   let negProduct = nums[0];
+  let positiveProduct = nums[0];
+  let result = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    const product = nums[i] * nums[i - 1];
-    const minProduct = Math.min(nums[i], product);
-    negProduct = nums[i] * negProduct;
-    nums[i] = Math.max(Math.max(product, nums[i]), negProduct);
-    negProduct = Math.min(minProduct, negProduct);
+    const curMax = Math.max(
+      nums[i] * positiveProduct,
+      nums[i],
+      nums[i] * negProduct
+    );
+    const curMin = Math.min(
+      nums[i] * positiveProduct,
+      nums[i],
+      nums[i] * negProduct
+    );
+    negProduct = curMin;
+    positiveProduct = curMax;
+    result = Math.max(result, positiveProduct);
   }
-  return nums;
+  return result;
 };
 
 // Example 1:
